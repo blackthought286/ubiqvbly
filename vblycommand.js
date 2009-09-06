7c0004cfb79ab3af963d94a94ebe419c1dd72f73
Everett <blackthought286@gmail.com>
2009-09-06 17:08:23 -0400

    refactored code so that shortened url would show up in the preview and you press enter to commit code to page

--- vblycommand.js
+++ vblycommand.js
@@ -1,9 +1,22 @@
+var vb_setURL = '';
+
 CmdUtils.CreateCommand({
  name: ["vb.ly", "vbly"],
  arguments: [{role: "object", label: "status", nountype: noun_arb_text}],
  preview: function show_url(pblock, args){
    var q = "Show URL selected: " + args.object.text;
    pblock.innerHTML = q;
+   
+   	var vbType = this._vbSetType("shorturl");
+	var seturl = vbType[0].url+'?action='+ vbType[0].action + '&url='+args.object.text+'&format=simple';
+   
+   if(seturl){
+		jQuery.get( seturl, function( show ) {
+			//CmdUtils.setSelection( show );
+			pblock.innerHTML = show;
+			vb_setURL = show;
+		})
+   }
  },
  _vbSetType: function vbSetType__vbSetType(results){
    var url = "http://vb.ly/api/";
@@ -19,11 +32,6 @@ CmdUtils.CreateCommand({
   }
  },
  execute: function(args){
-   
-   var vbType = this._vbSetType("shorturl");
-   var seturl = vbType[0].url+'?action='+ vbType[0].action + '&url='+args.object.text+'&format=simple';
-   jQuery.get( seturl, function( show ) {
-      CmdUtils.setSelection( show );
-    })
+         CmdUtils.setSelection( vb_setURL );
  }
 });
