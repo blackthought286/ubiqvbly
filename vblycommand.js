8c8658addfa2a7f9d0b85cd9f4b8a3e690288fdf
Everett Bryant <blackthought286@gmail.com>
2009-09-06 04:44:28 -0400

    This is the intial commit of vbly command

---
+++ vblycommand.js
@@ -0,0 +1,29 @@
+CmdUtils.CreateCommand({
+ name: ["vb.ly", "vbly"],
+ arguments: [{role: "object", label: "status", nountype: noun_arb_text}],
+ preview: function show_url(pblock, args){
+   var q = "Show URL selected: " + args.object.text;
+   pblock.innerHTML = q;
+ },
+ _vbSetType: function vbSetType__vbSetType(results){
+   var url = "http://vb.ly/api/";
+   var short_url = "shorturl";
+   var stats = "stats";
+   var format_xml = "xml";
+   var format_json = "json";
+
+  if(results != stats){
+    return settings = [{'url' : url, 'action' : short_url, 'format' : format_json }];
+  }else{
+    return settings = [{'url' : url, 'action' : stats, 'format' : format_json }];
+  }
+ },
+ execute: function(args){
+   
+   var loo = this._vbSetType("shorturl");
+   var seturl = loo[0].url+'?action='+loo[0].action + '&url='+args.object.text+'&format=simple';
+   jQuery.get( seturl, function( show ) {
+      CmdUtils.setSelection( show );
+    })
+ }
+});
